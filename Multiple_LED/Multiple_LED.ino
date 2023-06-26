8a609009ddc9520e7d5c1234a5d89b60b7a29a53
Peter Ramos <dchamp16@gmail.com>
2023-06-26 00:34:24 -0600

    Update Multiple_LED.ino

--- Multiple_LED/Multiple_LED.ino
+++ Multiple_LED/Multiple_LED.ino
@@ -1,34 +1,47 @@
-int temprature = 20;
+static int temprature;
 int ledRed = 4;
 int ledGreen = 3;
 int ledBlue = 2;
 
 void setup()
 {
+
   pinMode(ledRed, OUTPUT);
   pinMode(ledGreen, OUTPUT);
   pinMode(ledBlue, OUTPUT);
   Serial.begin(9600);
-  
-  //run once
-   if(temprature > 50){
-    Serial.println("Red LED turnon");
-  } else if(temprature <= 50 && temprature >=30){
-    Serial.println("green LED turnon");
-  } else {
-    Serial.println("blue LED turnon");
-  }
 
+  temprature = random(100);
+  Serial.println(temprature);
+
+  // run once
+  if (temprature > 50)
+  {
+    Serial.println("Red LED turnon: " + temprature);
+  }
+  else if (temprature <= 50 && temprature >= 30)
+  {
+    Serial.println("green LED turnon: " + temprature);
+  }
+  else
+  {
+    Serial.println("blue LED turnon: " + temprature);
+  }
 }
 
 void loop()
 {
   // runs multiple times
-  if(temprature > 50){
+  if (temprature > 50)
+  {
     digitalWrite(ledRed, HIGH);
-  } else if(temprature <= 50 && temprature >=30){
+  }
+  else if (temprature <= 50 && temprature >= 30)
+  {
     digitalWrite(ledGreen, HIGH);
-  } else {
+  }
+  else
+  {
     digitalWrite(ledBlue, HIGH);
   }
 }
